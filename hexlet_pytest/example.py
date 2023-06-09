bd50cce8f1b3c0136818f91966f8c9b49dc68a29
macbook <ramisphoto>
2023-06-09 12:17:38 +0300

    testing tests

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,12 @@
 def reverse(string):
     return string[::-1]
+
+
+stack = []
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+stack.pop()
+stack.pop()
---
+++ tests/test_stack.py
@@ -0,0 +1,10 @@
+from hexlet_pytest.example import stack
+
+
+def test_stack():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
